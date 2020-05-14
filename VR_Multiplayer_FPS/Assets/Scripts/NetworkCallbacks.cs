06dc0cff54abc881cd2566ac5db5092d20dae3c6
ParisiDesign <ben@parisidesign.com>
2020-05-13 18:55:11 -0700

    photon bolt scene with cube tutorial

---
+++ VR_Multiplayer_FPS/Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}
