3a147b8446b75dfad3dae660d84c2e262b7003a8
ParisiDesign <ben@parisidesign.com>
2020-05-15 20:02:44 -0700

    weapon switching on the cubes

--- VR_Multiplayer_FPS/Assets/Scripts/CubeBehaviour.cs
+++ VR_Multiplayer_FPS/Assets/Scripts/CubeBehaviour.cs
@@ -8,6 +8,7 @@ public class CubeBehaviour : Bolt.EntityEventListener<ICubeState>
 
     private float _resetColorTime;
     private Renderer _renderer;
+    public GameObject[] WeaponObjects;
 
     public override void Attached()
     {
@@ -20,7 +21,21 @@ public class CubeBehaviour : Bolt.EntityEventListener<ICubeState>
             state.CubeColor = new Color(Random.value, Random.value, Random.value);
         }
 
+        // On the owner, we want to setup the weapons, the Id is set just as the index
+        // and the Ammo is randomized between 50 to 100
+        for (int i = 0; i < state.WeaponArray.Length; ++i)
+        {
+            state.WeaponArray[i].WeaponId = i;
+            state.WeaponArray[i].WeaponAmmo = Random.Range(50, 100);
+        }
+
+        // by default we don't have any weapon up, so set index to -1
+        state.WeaponActiveIndex = -1;
+
         state.AddCallback("CubeColor", ColorChanged);
+
+        // we also setup a callback for whenever the index changes
+        state.AddCallback("WeaponActiveIndex", WeaponActiveIndexChanged);
     }
 
     void ColorChanged()
@@ -37,6 +52,12 @@ public class CubeBehaviour : Bolt.EntityEventListener<ICubeState>
         if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
         if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
         if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        // Input polling for weapon selection
+        if (Input.GetKeyDown(KeyCode.Alpha1)) state.WeaponActiveIndex = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) state.WeaponActiveIndex = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) state.WeaponActiveIndex = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha0)) state.WeaponActiveIndex = -1;
 
         if (movement != Vector3.zero)
         {
@@ -59,9 +80,23 @@ public class CubeBehaviour : Bolt.EntityEventListener<ICubeState>
 
     void Update()
     {
-        if (_resetColorTime < Time.time)
+        if (_renderer != null && _resetColorTime < Time.time)
         {
             _renderer.material.color = state.CubeColor;
+        }
+    }
+
+    void WeaponActiveIndexChanged()
+    {
+        for (int i = 0; i < WeaponObjects.Length; ++i)
+        {
+            WeaponObjects[i].SetActive(false);
+        }
+
+        if (state.WeaponActiveIndex >= 0)
+        {
+            int objectId = state.WeaponArray[state.WeaponActiveIndex].WeaponId;
+            WeaponObjects[objectId].SetActive(true);
         }
     }
 }
