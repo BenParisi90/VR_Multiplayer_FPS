1b8a4316e86a05407dcf093d1335aa837bd716fd
ParisiDesign <ben@parisidesign.com>
2020-05-22 11:17:49 -0700

    add muzzles flashes, and only fire gun that belongs to the locally owned avatar.

--- VR_FPS_Normcore/Assets/Scripts/GunController.cs
+++ VR_FPS_Normcore/Assets/Scripts/GunController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
 
-public class GunController : MonoBehaviour
+public class GunController : RealtimeComponent
 {
     bool triggerReleased = true;
     public Transform shootPoint;
+    public ParticleSystem muzzleFlashPrefab;
+    public RealtimeTransform avatarRealtimeTransform;
     // Update is called once per frame
     void Update()
     {
-        if(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > .5 && triggerReleased)
+        if(avatarRealtimeTransform.isOwnedLocally && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > .5 && triggerReleased)
         {
+            //add a muzzle flash for all players
+            Realtime.Instantiate(muzzleFlashPrefab.name, shootPoint.position, shootPoint.rotation);
+            //if the bullet hit something
             RaycastHit hit;
             if(Physics.Raycast(shootPoint.position, shootPoint.forward, out hit))
             {
@@ -20,6 +26,7 @@ public class GunController : MonoBehaviour
                     hit.transform.GetComponent<TargetController>().TakeDamage();
                 }
             }
+            //record that the trigger is held down
             triggerReleased = false;
         }
         else if(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) < .5 && !triggerReleased)
